12d267a0d8d5aa1c5dc400a1a65eecef17daac19
Lucas <lucascomaru20@gmail.com>
2022-06-04 17:47:33 -0300

    Tamanho da interface ajustado!

--- projeto_login.py
+++ projeto_login.py
@@ -2,8 +2,8 @@ import PySimpleGUI as sg
 
 #Layout
 sg.theme('Reddit')
-layout = [[sg.Text('Usuário'), sg.Input(key='usuario')],
-        [sg.Text('Senha'), sg.Input(key='senha', password_char='*')],
+layout = [[sg.Text('Usuário'), sg.Input(key='usuario', size=(20,1))],
+        [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(20,1))],
         [sg.Checkbox('Salvar o login? ')],
         [sg.Button('Entrar')]]
 
